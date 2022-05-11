168e2aaeac8f70cc219ab8968c63c57206673989
Nesterov Vlad <nesterovvlad100@gmail.com>
2022-05-11 13:13:44 +0300

    kak delat pridumal. ostalos sdelat

--- vasunin_8sem_3laba/Source.cpp
+++ vasunin_8sem_3laba/Source.cpp
@@ -2,10 +2,28 @@
 #include <cmath>
 using namespace std;
 
-
+int Fib_Num[10][100];
+int m;
 int main() {
+	//delay cherez stroki. schitai chislo i zapisivay v end stroki. kak stroka perepolnyaetsya obnulyai ee, uvelichivai counter
+	// i zapolnyai dalwe. ostanovis kogda doidew do iskomogo elemeta ravnogo polu4ennomu chislu - (counter * max dlinu stroki)
+	// glavnaya problema v zapisi chisel v end stroki. Esli ostavshihsya svobodnih mest < razmera chisla
+	cout << " vvedite nomer jelaemoy cifry" << endl;
+	cin >> m;
 
-	//cheres formulu beneya  ili 4erez vozvedenie v stepen matrix [[1,1],[1,0]]
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < 10; j++) {
+
+			if (true)
+			{
+
+			}
+
+		}
+
+	}
+
 
 	return 1;
 }
